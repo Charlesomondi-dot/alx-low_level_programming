e4912b1f6de0b7f103a9c9894708720002cfe89a
Charlesomondi-dot <charlesomondi232@gmail.com>
2023-09-08 02:43:28 +0300

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
   *main - Entry point
   *Description: this is the main function
   *that prints a message to a standard output.
-  *Return : always 0 (success)
+  *Return : Always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
