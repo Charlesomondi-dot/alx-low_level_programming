34a17543c4baf3f74be7841c152b6933fbae0190
Charlesomondi-dot <charlesomondi232@gmail.com>
2023-09-13 02:01:17 +0300

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,17 +12,17 @@ int main(void)
 	long a = 1;
 	long b = 2;
 
-		for (counter = 1; counter <= (countto / 2); counter++)
-		{
-			printf("%li, %li ", a, b);
-			printf(",");
-			a += b;
-			b += a;
-		}
-		if (countto % 2 == 1)
-		printf("%li", a);
+	printf("%li", a);
 
-		printf("\n");
+	for (counter = 1; counter <= (countto /2); counter++)
+	{
+		printf(", %li", b);
+		a+= b;
+		b+= a;
+	}
 
-		return (0);
+	printf("\n");
+
+	return (0);
+
 }
