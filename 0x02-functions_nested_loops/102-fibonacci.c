26fc60b6cd8c0617ec7316959743510dfecd79b4
Charlesomondi-dot <charlesomondi232@gmail.com>
2023-09-13 02:49:33 +0300

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 
-/**
- * main - print first 50 Fibonacci numbers
- * Return: 0
- */
-
 int main(void)
 {
-	int counter;
-	int countto = 50;
-	long a = 1;
-	long b = 2;
+	    int counter;
+	        int countto = 50;
+		    long a = 1;
+		        long b = 2;
 
+			    // Handle the first number separately
+			    printf("%li", a);
 
-	for (counter = 1; counter <= (countto / 2); counter++)
-	{
-		printf("%li, %li ", a, b);
-		printf(", ");
-		a += b;
-		b += a;
-	}
-	if (countto % 2 == 1)
-		printf(" %li", a);
+			        int charsPrinted = 1; // Initialize with the length of the first number
 
-	printf("\n");
+				    for (counter = 2; counter <= countto; counter++)
+					        {
+							        long temp = a + b;
+								        int numLength = snprintf(NULL, 0, "%li", temp); // Calculate the length of the next number
+									        charsPrinted += numLength; // Add the length of the next number
 
-	return (0);
+										        if (charsPrinted + numLength + 2 <= 384) // Check if there's enough space for the next number and a comma and space
+												        {
+														            printf(", %li", temp); // Print numbers with a comma
+															                charsPrinted += 2; // Add the length of the comma and space
+																	        }
+											        else
+													        {
+															            break; // Stop if adding the next number would exceed 384 characters
+																            }
 
+												        a = b;
+													        b = temp;
+														    }
+
+				        printf("\n");
+
+					    return 0;
 }
+
