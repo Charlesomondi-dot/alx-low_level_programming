8532dec9c7af1f96b7f5c0714d159ed49d87aa70
Charlesomondi-dot <charlesomondi232@gmail.com>
2023-10-04 02:53:26 +0300

    C - malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,10 +9,10 @@
  */
 int count_word(char*s)
 {
-	int flag,c;
+	int flag,c,w;
 
 	flag = 0;
-	int w = 0;
+	w = 0;
 	
 	for(c = 0; s[c] !='\0';c++)
 	{
@@ -33,20 +33,22 @@ int count_word(char*s)
  * Return: pointer to an array of strings(Success)
  * or NULL (Error)
  */
-char **strtow(char*str)
+char **strtow(char *str)
 {
-	char**matrix, *tmp;
+	char **matrix = 0, *tmp;
 	int i,k = 0,len = 0, words, c = 0, start, end;
 	
 	while(*(str + len))
 		len++;
 	words = count_word(str);
-	if( matrix == 0)
+	if(matrix == 0) {
 		return (NULL);
+	}
 
 	matrix = (char**)malloc(sizeof(char*)*(words + 1));
-	if(matrix == NULL)
-	return (NULL);
+	if(matrix == NULL) {
+	return NULL;
+	}
 
 	for(i = 0;i <= len;i++)
 	{
