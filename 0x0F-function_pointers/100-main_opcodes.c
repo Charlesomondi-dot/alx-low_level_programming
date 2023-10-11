eb9533a6c0f789edb2872ea3fa8a92b2c4de15a3
Charlesomondi-dot <charlesomondi232@gmail.com>
2023-10-12 02:36:55 +0300

    C - Function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_opcodes(unsigned char *main_ptr, int num_bytes) {
-	    for (int i = 0; i < num_bytes; i++) {
-		            printf("%02x", main_ptr[i]);
-			            if (i < num_bytes - 1) {
-					                printf(" ");
-							        }
-				        }
-	        printf("\n");
-}
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main(int argc, char *argv[]) {
-	    if (argc != 2) {
-		            printf("Error\n");
-			            return 1;
-				        }
+int main(int argc, char *argv[]) 
+{
+	int bytes, i;
+	char *arr;
 
-	        int num_bytes = atoi(argv[1]);
-		    if (num_bytes <= 0) {
-			            printf("Error\n");
-				            return 2;
-					        }
+	if(argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
 
-		        unsigned char *main_ptr = (unsigned char *)main;
-			    print_opcodes(main_ptr, num_bytes);
-
-			        return 0;
-}
+	if(bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
+	
+	for(i = 0; i < bytes; i++)
+	{
+		if(i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i];
+					break;
+					}
+					printf("%hhx", arr[i]);
+					}
+					return(0);
+					}
 
