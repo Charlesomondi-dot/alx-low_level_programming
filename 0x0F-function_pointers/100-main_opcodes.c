8a0957b507e502027cfe3dc7d56609af0e766d2a
Charlesomondi-dot <charlesomondi232@gmail.com>
2023-10-12 01:37:41 +0300

    C - Function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,28 +2,31 @@
 #include <stdlib.h>
 
 void print_opcodes(unsigned char *main_ptr, int num_bytes) {
-	for (int i = 0; i < num_bytes; i++) {
-		printf("%02x", main_ptr[i]);
-	if (i < num_bytes - 1) {
-		printf(" ");
-		}
-	}
-	printf("\n");
+	    for (int i = 0; i < num_bytes; i++) {
+		            printf("%02x", main_ptr[i]);
+			            if (i < num_bytes - 1) {
+					                printf(" ");
+							        }
+				        }
+	        printf("\n");
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-	printf("Error\n");
-		return 1;
-	}
+	    if (argc != 2) {
+		            printf("Error\n");
+			            return 1;
+				        }
 
-	int num_bytes = atoi(argv[1]);
-	if (num_bytes < 0)
+	        int num_bytes = atoi(argv[1]);
+		    if (num_bytes < 0) {
+			            printf("Error\n");
+				            return 2;
+					        }
+
+		        unsigned char *main_ptr = (unsigned char *)main;
+			    print_opcodes(main_ptr, num_bytes);
+
+			        return 0;
 }
 
-unsigned char *main_ptr = (unsigned char *)main;
-	print_opcodes(main_ptr, num_bytes);
-
-	return 0;
-}
 
