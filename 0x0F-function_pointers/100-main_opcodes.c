7a4c7034766761f93949a5d39c28a0be3ea0c6cd
Charlesomondi-dot <charlesomondi232@gmail.com>
2023-10-12 02:57:30 +0300

    C - Function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,34 +8,33 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[]) 
 {
 	int bytes, i;
 	char *arr;
 
-	if(argc != 2)
+	if(argc != 2) 
 	{
 		printf("Error\n");
 		exit(1);
 	}
+
 	bytes = atoi(argv[1]);
 
-	if(bytes < 0)
+	if(bytes < 0) 
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	arr = (char *)main;
+	arr =(char *) main;
 	
 	for(i = 0; i < bytes; i++)
 	{
 		if(i == bytes - 1)
 		{
 			printf("%02hhx\n", arr[i]);
-					break ;
+				break;
 		}
-
 		printf("%hhx", arr[i]);
 	}
 	return(0);
