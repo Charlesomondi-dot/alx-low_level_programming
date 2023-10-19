ed5de9dbb13e0d59fc28a99543f21b00fec9eb7f
Charlesomondi-dot <charesomondi232@gmail.com>
2023-10-20 00:42:47 +0300

    singly_linked_lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,6 @@
 #include <stdio.h>
 
 int main(){ 
-	// Inline assembly to call printf with the message "Hello, Holberton\n"
-	asm("mov $1, %%rax\n"   // RAX = 1 (syscall number for write)
-		"mov $1, %%rdi\n"   // RDI = 1 (file descriptor for stdout)
-		"mov $hello, %%rsi\n" // RSI = address of the hello string
-		"mov $16, %%rdx\n"   // RDX = length of the hello string
-		"syscall"            // Invoke syscall to write to stdout
-		:
-		: "r"(hello)         // Input operand: address of hello string
-		: "%rax", "%rdi", "%rsi", "%rdx"  // Clobbered registers
-	);
-
-	        return 0;
+	printf("Hello, Holberton");
 }
 
